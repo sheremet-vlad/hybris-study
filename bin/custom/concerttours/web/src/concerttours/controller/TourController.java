fa547de651138a7e7e213d4f849ee194b18dd5ba
vlad <ysc.vlad@gmail.com>
2019-06-14 14:17:21 +0300

    add front-end to concerttours

---
+++ bin/custom/concerttours/web/src/concerttours/controller/TourController.java
@@ -0,0 +1,40 @@
+package concerttours.controller;
+
+import concerttours.data.TourData;
+import concerttours.facades.TourFacade;
+import de.hybris.platform.catalog.CatalogVersionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+@Controller
+public class TourController {
+    private static final String CATALOG_ID = "concertoursProductCatalog";
+    private static final String CATALOG_VERSION_NAME = "Online";
+    private CatalogVersionService catalogVersionService;
+    private TourFacade tourFacade;
+
+    @RequestMapping(value = "/tours/{tourId}")
+    public String showTourDetails(@PathVariable final String tourId, final Model model) throws UnsupportedEncodingException {
+        catalogVersionService.setSessionCatalogVersion(CATALOG_ID, CATALOG_VERSION_NAME);
+        final String decodedTourId = URLDecoder.decode(tourId, "UTF-8");
+        final TourData tour = tourFacade.getTourDetails(decodedTourId);
+        model.addAttribute("tour", tour);
+        return "TourDetails";
+    }
+
+    @Autowired
+    public void setCatalogVersionService(final CatalogVersionService catalogVersionServiceService) {
+        this.catalogVersionService = catalogVersionServiceService;
+    }
+
+    @Autowired
+    public void setFacade(final TourFacade facade) {
+        this.tourFacade = facade;
+    }
+}
